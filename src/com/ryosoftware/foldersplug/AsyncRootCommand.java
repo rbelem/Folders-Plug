a9f07b2b727e87a25d3b166d06e7382b69e4427d
Rodrigo Belem <rodrigo.belem@gmail.com>
2013-06-04 19:16:22 -0400

    Use AsyncTask to run commands, such as mv, ls and stat.
    Signed-off-by: Rodrigo Belem <rodrigo.belem@gmail.com>

--- src/com/ryosoftware/foldersplug/AsyncRootCommand.java
+++ src/com/ryosoftware/foldersplug/AsyncRootCommand.java
@@ -1,19 +1,78 @@
 
 package com.ryosoftware.foldersplug;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.AsyncTask;
+
+import java.util.List;
 
 import eu.chainfire.libsuperuser.Shell;
 
-public class AsyncRootCommand extends AsyncTask<String, Void, Boolean> {
+public class AsyncRootCommand extends AsyncTask<String, Void, List<String>> {
+    private Activity m_activity;
+    private AsyncRootCommandListener m_listener;
+    private ProgressDialog m_dialog = null;
+    private List<String> m_result = null;
+    private String m_dialogTitle = "";
+    private String m_dialogMessage = "";
+
+    public AsyncRootCommand() {
+        this.m_activity = null;
+        this.m_listener = null;
+    }
+
+    public AsyncRootCommand(Activity activity) {
+        this.m_activity = activity;
+    }
+
+    public void setListener(AsyncRootCommandListener listener) {
+        this.m_listener = listener;
+    }
+
+    public void setDialogTitle(String title) {
+        this.m_dialogTitle = title;
+    }
+
+    public void setDialogMessage(String message) {
+        this.m_dialogMessage = message;
+    }
+
+    public List<String> result() {
+        return m_result;
+    }
 
     @Override
-    protected Boolean doInBackground(String... params) {
-        if (Shell.SU.available()) {
-            Shell.SU.run(params);
-            return true;
+    protected void onPreExecute() {
+        if (m_activity == null) {
+            return;
         }
 
-        return false;
+        m_dialog = new ProgressDialog(m_activity);
+        m_dialog.setTitle(m_dialogTitle);
+        m_dialog.setMessage(m_dialogMessage);
+        m_dialog.setIndeterminate(true);
+        m_dialog.setCancelable(false);
+        m_dialog.show();
+    }
+
+    @Override
+    protected List<String> doInBackground(String... params) {
+        if (Shell.SU.available()) {
+            m_result = Shell.SU.run(params);
+            return m_result;
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> result) {
+        if (m_activity != null) {
+            m_dialog.dismiss();
+        }
+        if (m_listener != null) {
+            m_listener.onCommandCompleted(result);
+        }
     }
 }
---
+++ src/com/ryosoftware/foldersplug/AsyncRootCommandListener.java
@@ -0,0 +1,7 @@
+
+package com.ryosoftware.foldersplug;
+import java.util.List;
+
+public interface AsyncRootCommandListener {
+    public void onCommandCompleted(List<String> result);
+}
